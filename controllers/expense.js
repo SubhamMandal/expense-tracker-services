830289f704cad26c6370fd9383ee310bfc7c101b
SubhamMandal <iamsubhammandal.sm@gmail.com>
2024-02-05 18:35:18 +0530

    update: expense controller

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,4 +1,6 @@
 const Expense = require('../models/expense');
+const User = require('../models/user');
+const Group = require('../models/group');
 
 exports.addExpense = async (req, res, next) => {
     const expense = new Expense({
@@ -28,7 +30,9 @@ exports.addExpense = async (req, res, next) => {
 exports.allExpense = async (req, res, next) => {
     let allExpense;
     try {
-        allExpense = await Expense.find({ creator: req.userId });
+        allExpense = await Expense.find({ $or: [{ "splitAmount.userId": req.userId }, { paidBy: req.userId }, { $and: [{ associatedGroup: null }, { creator: req.userId }] }] });
+        // allExpense = await Expense.find({ creator: req.userId });
+        // allExpense = await Expense.find({$and: [{ paidBy: req.userId }]});
     } catch (err) {
         err.statusCode = 500;
         return next(err);
@@ -38,12 +42,23 @@ exports.allExpense = async (req, res, next) => {
 
 exports.expenseDetails = async (req, res, next) => {
     const expenseId = req.params.expenseId;
+    const response = {};
     let expense;
+    let users;
+    let group;
     try {
         expense = await Expense.findOne({ _id: expenseId });
+        response.expenseData = expense;
+        if (expense.associatedGroup) {
+            const members = expense.splitAmount.map(data => data.userId);
+            users = await User.find({ $or: [{ _id: { $in: members } }, { _id: expense.paidBy }] }, { username: 1 });
+            group = await Group.findOne({ _id: expense.associatedGroup }, { _id: 0, category: 1 });
+            response.catagory = group.category;
+            response.users = users;
+        }
     } catch (err) {
         err.statusCode = 500;
         return next(err);
     }
-    return await res.status(200).json({ expenseData: expense, message: 'expense data fetched successfully' });
+    return await res.status(200).json({ ...response, message: 'expense data fetched successfully' });
 }
